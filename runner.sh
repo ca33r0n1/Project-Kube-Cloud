414370b0495cd6750a773547f1545ea28163641d
Ca33r0n1 <ca33r0n1@users.noreply.github.com>
2022-01-31 17:27:45 +0000

    Update on Runner.sh

--- runner.sh
+++ runner.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting Server Install Script"
 wget -O installer.sh "https://${weburl}/${servergroup}/${servertype}-install.sh"
-/bin/sh /data/installer.sh ${jenkinskey}
+/bin/sh  -c "/data/installer.sh ${jenkinskey}"
