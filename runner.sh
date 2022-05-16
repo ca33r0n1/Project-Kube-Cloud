b20564359ae49c0f54419e187869d894bce6640b
Connor <84043753+readUTF@users.noreply.github.com>
2022-05-16 21:04:41 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Starting Server Install Script"
-wget --no-check-certificate -O installer.sh "https://${weburl}/${servergroup}/${servertype}-install.sh"
+wget --no-check-certificate -O installer.sh "https://${weburl}/MPL-Server/Scripts/${servergroup}/install.sh"
 chmod +x "installer.sh"
 /bin/sh  -c "/data/installer.sh ${jenkinskey}"
