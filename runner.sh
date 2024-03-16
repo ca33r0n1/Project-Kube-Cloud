233b62e00140b157e5cf3063df8d5e6a600efd7a
Ca33r0n1 <ca33r0n1@users.noreply.github.com>
2024-03-16 17:37:03 +0000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Starting Server Install Script"
-wget --no-check-certificate -O installer.sh "https://${weburl}/MPL-Server/Scripts/${servergroup}/install.sh"
+wget --no-check-certificate -O installer.sh "${weburl}/${servergroup}/install.sh"
 chmod +x "installer.sh"
 /bin/sh  -c "/data/installer.sh ${jenkinskey}"
